9c86c1e8712fc494ba52996b1f1d9f6d957f2db5
Felix Schmidt <Felix-schmidt2@outlook.de>
2024-09-09 16:30:12 +0200

    add check for folder start with .

--- setup.sh
+++ setup.sh
@@ -5,20 +5,21 @@ dotfiles_dir="$(dirname "$0")"
 
 # Home directory
 home_dir="$HOME"
-
 # Iterate over all files starting with .
 for file in "$dotfiles_dir"/.*; do
     # Exclude . and ..
     if [[ "$file" != "$dotfiles_dir/." && "$file" != "$dotfiles_dir/.." ]]; then
-        # Create symlink in the home directory
-        ln -s "$file" "$home_dir/$(basename "$file")"
+        # Check if file is not a folder
+        if [[ ! -d "$file" ]]; then
+            # Create symlink in the home directory
+            ln -s "$file" "$home_dir/$(basename "$file")"
+        fi
     fi
 done
 
-# Function to append variable to bashrc file
-append_to_bashrc() {
+export_file_into_bashrc() {
     local variable_name="$1"
-    echo ". $variable_name" >> "$home_dir/.bashrc"
+    echo ". .$variable_name" >> "$home_dir/.bashrc"
 }
 
-append_to_bashrc ".show_git_branch"
+export_file_into_bashrc ".show_git_branch"
